fac460df49388957c384a2022335cc5b943f54a8
Aaron <awh66@cornell.edu>
2018-02-04 02:37:28 +0000

    Added --help functionality

--- src/xic/Xic.java
+++ src/xic/Xic.java
@@ -8,25 +8,42 @@ public class Xic {
     public static void main(String [] args) {
         if (args.length > 1 && args[0].equals("--lex")) {
             try {
-                String source = args[1];
-                String output = FilenameUtils.removeExtension(source) + ".lexed";
-                BufferedWriter w = new BufferedWriter(new FileWriter(output, false));
-                try {
-                    XiLexer lexer = new XiLexer(new FileReader(source));
-                    Token t = lexer.nextToken();
-                    while (t.type != TokenType.EOF) {
-                        w.append(t.toString() + "\n");
-                        t = lexer.nextToken();
+                for(int i=1;i<args.length;i++) {
+                    String source = args[1];
+                    String output = FilenameUtils.removeExtension(source) + ".lexed";
+                    BufferedWriter w = new BufferedWriter(new FileWriter(output, false));
+                    try {
+                        XiLexer lexer = new XiLexer(new FileReader(source));
+                        Token t = lexer.nextToken();
+                        while (t.type != TokenType.EOF) {
+                            w.append(t.toString() + "\n");
+                            t = lexer.nextToken();
+                        }
+                        w.close();
+                    } catch (Exception e) {
+                        w.append(e.getMessage());
+                        w.close();
+                        System.out.println(e.getMessage());
                     }
-                    w.close();
-                } catch (Exception e) {
-                    w.append(e.getMessage());
-                    w.close();
-                    System.out.println(e.getMessage());
-                }
+                } 
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        } 
+
+        }
+        else if (args.length == 1 && args[0].equals("--help")) {
+            try {
+                BufferedReader in = new BufferedReader(new FileReader("resources/help.txt"));
+                String line = in.readLine();
+                while(line != null)
+                {
+                  System.out.println(line);
+                  line = in.readLine();
+                }
+                in.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
