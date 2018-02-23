8995792b91e89314de8462b1aff2beb6ef47deea
Newton Ni <nwtnni@gmail.com>
2018-02-22 23:40:56 -0500

    Added static constants for primitive types

--- src/main/java/type/Type.java
+++ src/main/java/type/Type.java
@@ -4,6 +4,11 @@ import ast.XiType;
 
 public class Type {
 
+    public static final Type INT = new Type("int");
+    public static final Type BOOL = new Type("bool");
+    public static final Type UNIT = new Type("unit");
+    public static final Type VOID = new Type("void");
+
     public enum Kind {
         ARRAY, CLASS,
     }
@@ -11,6 +16,12 @@ public class Type {
     public Kind kind;
     public String id;
     public Type child;
+
+    private Type(String id) {
+        this.kind = Kind.CLASS;
+        this.id = id; 
+        this.child = null;
+    }
     
     public Type(XiType xt) {
         if (xt.isClass()) {
