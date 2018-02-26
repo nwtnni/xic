75556caed907755dfcc981e491b9209fc3506d48
Newton Ni <nwtnni@gmail.com>
2018-02-25 21:07:11 -0500

    Updated Type to include tuples; renamed Resolver to UseImporter

--- src/main/java/type/Type.java
+++ src/main/java/type/Type.java
@@ -1,5 +1,7 @@
 package type;
 
+import java.util.ArrayList;
+
 import ast.XiType;
 
 public class Type {
@@ -13,65 +15,80 @@ public class Type {
     public static final Type[] TYPES = {INT, BOOL, UNIT, VOID, POLY};
 
     public enum Kind {
-        ARRAY, CLASS,
+        ARRAY, CLASS, TUPLE
     }
 
     public Kind kind;
     public String id;
-    public Type child;
+    public ArrayList<Type> children;
 
     private Type(String id) {
         this.kind = Kind.CLASS;
         this.id = id;
-        this.child = null;
+        this.children = null;
     }
 
     public Type(Type child) {
         this.kind = Kind.ARRAY;
         this.id = null;
-        this.child = child;
+        this.children = new ArrayList<>();
+        this.children.add(child);
+    }
+    
+    public Type(ArrayList<Type> children) {
+    	this.kind = Kind.TUPLE;
+    	this.id = null;
+    	this.children = children;
     }
 
     public Type(XiType xt) {
         if (xt.isClass()) {
             this.kind = Kind.CLASS;
             this.id = xt.id;
-            this.child = null;
+            this.children = null;
         } else {
             this.kind = Kind.ARRAY;
             this.id = null;
-            this.child = new Type((XiType) xt.child);
+            this.children = new ArrayList<>(); 
+            children.add(new Type((XiType) xt.child));
         }
     }
-
-    public boolean isClass() {
-        return kind == Kind.CLASS;
-    }
-
+    
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof Type)) { return false; }
 
         Type t = (Type) o;
 
-        boolean a_class = isClass();
-        boolean b_class = t.isClass();
-
-        if (a_class && b_class) {
+        if (kind == Kind.CLASS && t.kind == Kind.CLASS) {
             return t.id.equals(id);
-        } else if (!a_class && !b_class) {
-            return t.child.equals(child);
+        } else if (kind == Kind.ARRAY && t.kind == Kind.ARRAY) {
+            return t.children.get(0).equals(children.get(0));
+        } else if (kind == Kind.TUPLE && t.kind == Kind.TUPLE) {
+        	
+        	if (children.size() != t.children.size()) { return false; }
+        	
+        	for (int i = 0; i < children.size(); i++) {
+        		if (!children.get(i).equals(t.children.get(i))) {
+        			return false;
+        		}
+        	}
+            return true;
         } else {
-            return false;
+        	return false;
         }
     }
 
     @Override
     public int hashCode() {
-        if (isClass()) {
+        if (kind == Kind.CLASS) {
             return id.hashCode();
         } else {
-            return 10 * child.hashCode();
+        	int hash = 1;
+        	for (Type child : children) {
+        		hash += 10 * child.hashCode();
+        	}
+        	return hash;
         }
     }
 }
