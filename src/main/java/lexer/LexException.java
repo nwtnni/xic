9991053c33365d7895abb9ac31492b696d6665e4
Ray Zeng <rz72@cornell.edu>
2018-02-28 02:46:28 -0500

    support for min_value checking added in parser

--- src/main/java/lexer/LexException.java
+++ src/main/java/lexer/LexException.java
@@ -12,22 +12,25 @@ public class LexException extends XicException {
 		IO("Failed to read next token from file"),
 		
 		// Lex errors
-		INVALID_CHAR_LITERAL("invalid char literal"),
-		INVALID_INT_LITERAL("invalid int literal"),
-		INVALID_ESCAPE_SEQUENCE("invalid escape sequence"),
-		INVALID_STRING_TERMINATOR("string literal not properly terminated"),
-		INVALID_SYNTAX("invalid syntax");
+		INVALID_CHAR_LITERAL("Invalid char literal"),
+		INVALID_ESCAPE_SEQUENCE("Invalid escape sequence"),
+		INVALID_STRING_TERMINATOR("String literal not properly terminated"),
+		INVALID_SYNTAX("Invalid syntax");
 		
 		private String description;
 		private Kind(String description) { this.description = description; }
 		public String toString() { return description; }
 	}
 	
-	public LexException(Kind kind) {
-		super(kind.toString());
-	}
-	
-    public LexException(ComplexSymbol symbol, Kind kind) {
+    public LexException(Kind kind, ComplexSymbol symbol) {
     	super(XicException.Kind.LEXICAL, symbol.getLeft(), kind.toString());
     }
+	
+	private LexException(Kind kind) {
+		super(kind.toString());
+	}
+
+	public static LexException internal(Kind kind) {
+		return new LexException(kind);
+	}
 }
