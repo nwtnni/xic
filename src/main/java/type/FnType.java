7bc19d6658134a162a7dd40f178a35f1c84c5ed3
Newton Ni <nwtnni@gmail.com>
2018-03-01 20:59:43 -0500

    Documentation for FnType

--- src/main/java/type/FnType.java
+++ src/main/java/type/FnType.java
@@ -6,18 +6,52 @@ import ast.*;
 import java_cup.runtime.ComplexSymbolFactory.Location;
 import xic.XicException;
 
+/**
+ * Represents a function type as defined the Xi Type Specification.
+ * 
+ * Functions can have any number of argument and return types. This
+ * class visits a {@link ast.Fn} node and its {@link ast.XiType} children
+ * in order to do its conversions.
+ * 
+ * @see Type
+ * @see ast.Visitor
+ */
 public class FnType extends Visitor<Type> {
 
+	/**
+	 * Convenience factory method to create a FnType from a Fn.
+	 * 
+	 * @param f Fn node to convert
+	 * @return FnType corresponding to Fn f
+	 */
 	public static FnType from(Fn f) {
 		FnType type = new FnType();
 		type.visit(f);
 		return type;
 	}
 
+	/**
+	 * Stores the Fn's location for error display.
+	 */
 	public Location location;
+	
+	/**
+	 * Stores the Fn's arguments. Can be a {@link Type.Kind#TUPLE} for
+	 * functions with zero or more than one argument types, or a {@link Type.Kind#CLASS}
+	 * for functions with one return type.
+	 */
 	public Type args;
+	
+	/**
+	 * Stores the Fn's return types. Can be a {@link Type.Kind#TUPLE} for
+	 * functions with zero or more than one return types, or a {@link Type.Kind#CLASS}
+	 * for functions with one return type.
+	 */
 	public Type returns;
 
+	/**
+	 * Visits a Fn's children.
+	 */
     public Type visit(Fn f) {
     	try {
     		location = f.location;
@@ -30,6 +64,9 @@ public class FnType extends Visitor<Type> {
     	return null;
     }
     
+    /**
+     * Visits a Multiple's children and collects them into a tuple.
+     */
     public Type visit(Multiple m) throws XicException {
     	if (m.values.size() == 0) { return Type.UNIT; }
     	ArrayList<Type> types = new ArrayList<>();
@@ -39,14 +76,24 @@ public class FnType extends Visitor<Type> {
     	return new Type(types);
     }
 
+    /**
+     * Visits a Declare node and returns its type.
+     */
     public Type visit(Declare d) throws XicException {
     	return d.xiType.accept(this);
     }
 
+    /**
+     * Visits a XiType node and converts it to a Type.
+     */
     public Type visit(XiType xt) {
     	return new Type(xt);
     }
 
+    /**
+     * Two functions are equal if their argument and return types
+     * are equal.
+     */
     @Override
     public boolean equals(Object o) {
     	if (!(o instanceof FnType)) { return false; }
@@ -54,6 +101,9 @@ public class FnType extends Visitor<Type> {
     	return args.equals(type.args) && returns.equals(type.returns);
     }
 
+    /**
+     * Implemented to maintain equivalence with equals.
+     */
     @Override
     public int hashCode() {
     	return args.hashCode() * returns.hashCode();
