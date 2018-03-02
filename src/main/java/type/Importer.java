ca28c0bba01b7e0a01e3c7a76df737164646981a
Newton Ni <nwtnni@gmail.com>
2018-03-01 21:19:19 -0500

    Importer documentation

--- src/main/java/type/Importer.java
+++ src/main/java/type/Importer.java
@@ -5,22 +5,66 @@ import parser.IXiParser;
 import type.TypeException.Kind;
 import xic.XicException;
 
+/**
+ * Subclass of {@link TypeCheck} which type checks top-level
+ * declarations of both interface (.ixi) and implementation (.xi)
+ * files.
+ * 
+ * Importer recursively visits {@code use} dependencies. Conceptually,
+ * this can be broken down into three phases:
+ * 
+ * 1) For each dependency, search for the .ixi file and parse into AST
+ * 2) For each function in the AST, add it to the {@link FnContext} if it is unique.
+ * 3) For each dependency, merge the FnContexts back into the original AST, checking
+ *    that shadowed functions have the same types.
+ *    
+ * Additionally, step 2 can be broken down into two passes:
+ * 
+ * 1) Add each function's types to the FnContext
+ * 2) Check all function arguments for shadowing against the top-level context
+ */
 public class Importer extends TypeCheck {
-    
+
+	/**
+	 * Factory method to resolve dependencies in an AST into a FnContext.
+	 * 
+	 * @param lib Directory to search for .ixi files
+	 * @param ast Parsed source AST to extract use dependencies from
+	 * @return Top-level function declarations in FnContext form
+	 * @throws XicException if any functions are illegally shadowed
+	 */
     public static FnContext resolve(String lib, Node ast) throws XicException {
     	Importer resolver = new Importer(lib);
     	ast.accept(resolver);
     	return resolver.fns;
     }
     
+    /**
+     * Directory to search for .ixi files
+     */
     private String lib;
+    
+    /**
+     * Record whether this Importer is on its first or second pass through the Fn nodes
+     */
     private boolean populate;
     
+    /**
+     * Creates a new Importer that will search the given directory
+     * 
+     * @param lib Directory to search for .ixi files
+     * @throws XicException if importing failed (e.g. invalid .ixi file
+     *   		or illegal function shadowing)
+     */
     private Importer(String lib) throws XicException {
     	this.lib = lib;
     	this.populate = true;
     }
     
+    /**
+     * Visit a {@link ast.Program} and extract its top-level function
+     * declarations into a {@link FnContext}
+     */
     @Override
     public Type visit(Program p) throws XicException {
     	if (p.isProgram()) {
@@ -46,6 +90,9 @@ public class Importer extends TypeCheck {
     	return null;
     }
     
+    /**
+     * Recursively visit dependencies.
+     */
     @Override
     public Type visit(Use u) throws XicException {
     	Node ast = IXiParser.from(lib, u.file + ".ixi");
@@ -53,6 +100,9 @@ public class Importer extends TypeCheck {
     	return null;
     }
     
+    /**
+     * Add function type information to top-level context.
+     */
     @Override
     public Type visit(Fn f) throws XicException {
     	if (!populate) {
