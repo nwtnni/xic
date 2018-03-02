6e17b2f2eb81738351d24a933403e965b7c08a3d
Newton Ni <nwtnni@gmail.com>
2018-03-01 21:22:59 -0500

    type.Printer documentation

--- src/main/java/type/Printer.java
+++ src/main/java/type/Printer.java
@@ -7,8 +7,21 @@ import ast.Node;
 import parser.XiParser;
 import xic.XicException;
 
+/**
+ * Convenience class to write the result of a type check run to file.
+ */
 public class Printer {
 	
+	/**
+	 * Parses the given file, runs the type checker, and outputs diagnostic
+	 * information to the given output file as well as System.out.
+	 * 
+	 * @param source Directory to search for the source
+	 * @param sink Directory to output the result
+	 * @param lib Directory to search for interface files
+ 	 * @param unit Path to the target source file, relative to source
+	 * @throws XicException if the Printer was unable to write to the given file
+	 */
 	public static void print(String source, String sink, String lib, String unit) throws XicException {
 		String ext = FilenameUtils.getExtension(unit);
     	String output = FilenameUtils.concat(sink, FilenameUtils.removeExtension(unit));
