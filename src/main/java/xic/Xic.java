0e41436383e3e9402bb5f7e4496b5f303d24c47a
Newton Ni <nwtnni@gmail.com>
2018-03-01 22:42:52 -0500

    Xic documentation

--- src/main/java/xic/Xic.java
+++ src/main/java/xic/Xic.java
@@ -7,18 +7,51 @@ import parser.IXiParser;
 import parser.XiParser;
 import type.TypeCheck;
 
+/**
+ * Main compiler class. Wraps around and provides convenience methods
+ * for every phase of compilation.
+ */
 public class Xic {
 	
-	private String source; // for source path
-	private String sink; // for output path
-	private String lib; // for use path - interfaces only
-
+	/**
+	 * The source path associated with this compiler, i.e. where it will
+	 * look for source files.
+	 */
+	private String source;
+	
+	/**
+	 * The sink path associated with this compiler, i.e. where it will
+	 * output diagnostic files.
+	 */
+	private String sink;
+	
+	/**
+	 * The lib path associated with this compiler, i.e. where it will
+	 * look for interface files.
+	 */
+	private String lib;
+	
+	/**
+	 * Creates a new compiler instance which will read from the given
+	 * file paths.
+	 * 
+	 * @param source Where to look for source files
+	 * @param sink Where to output diagnostic files
+	 * @param lib Where to look for interface files
+	 */
 	public Xic(String source, String sink, String lib) {
 		this.source = source;
 		this.sink = sink;
 		this.lib = lib;
 	}
 
+	/**
+	 * Creates a {@link lexer.XiLexer} which reads the input file.
+	 * 
+	 * @param unit The path to the input file, relative to source
+	 * @return A XiLexer instance which will read the input file
+	 * @throws XicException if lexing failed
+	 */
 	public XiLexer lex(String unit) throws XicException {
 		switch (FilenameUtils.getExtension(unit)) {
 			case "xi":
@@ -28,7 +61,14 @@ public class Xic {
 				throw XicException.unsupported(unit);
 		}
 	}
-	
+
+	/**
+	 * Returns the AST associated with the input file.
+	 * 
+	 * @param unit The path to the input file, relative to source
+	 * @return The AST of the input file
+	 * @throws XicException if lexing or parsing failed
+	 */
 	public Node parse(String unit) throws XicException {
 		Node ast = null;
 		
@@ -47,20 +87,47 @@ public class Xic {
 		return ast;
 	}
 	
+	/**
+	 * Returns the decorated AST (i.e. with type annotations) 
+	 * associated with the input file.
+	 * 
+	 * @param unit The path to the input file, relative to source
+	 * @return The decorated AST of the input file
+	 * @throws XicException if lexing, parsing, or typechecking failed
+	 */
 	public Node typeCheck(String unit) throws XicException {
 		Node ast = parse(unit);
 		TypeCheck.check(lib, ast);
 		return ast;
 	}
 	
+	/**
+	 * Prints and writes diagnostics for the lexed input file.
+	 * 
+	 * @param unit The path to the input file, relative to source
+	 * @throws XicException if lexing failed
+	 */
 	public void printLexed(String unit) throws XicException {
 		lexer.Printer.print(source, sink, unit);
 	}
 	
+	/**
+	 * Prints and writes diagnostics for the lexed and parsed input file.
+	 * 
+	 * @param unit The path to the input file, relative to source
+	 * @throws XicException if lexing or parsing failed
+	 */
 	public void printParsed(String unit) throws XicException {
 		parser.Printer.print(source, sink, unit);
 	}
 
+	/**
+	 * Prints and writes diagnostics for the lexed, parsed, and typechecked input
+	 * file.
+	 * 
+	 * @param unit The path to the input file, relative to source
+	 * @throws XicException if lexing, parsing, or typechecking failed
+	 */
 	public void printTyped(String unit) throws XicException {
 		type.Printer.print(source, sink, lib, unit);
 	}
