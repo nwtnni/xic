38bc75d4e0c6e248f5f79bcc6b718c42efa96642
ajt232 <ajt232@cornell.edu>
2018-03-12 01:32:43 -0400

    fixing variable names

--- src/main/java/emit/ConstantFolder.java
+++ src/main/java/emit/ConstantFolder.java
@@ -14,10 +14,10 @@ public class ConstantFolder extends IRVisitor<OptionalLong> {
 	public OptionalLong visit(IRBinOp b) {
 		OptionalLong ltol = b.left.accept(this);
         OptionalLong rtol = b.right.accept(this);
-        if (lt.isPresent() && rt.isPresent()) {
+        if (ltol.isPresent() && rtol.isPresent()) {
             long c;
-            long lt = lt.getAsLong();
-            long rt = rt.getAsLong();
+            long lt = ltol.getAsLong();
+            long rt = rtol.getAsLong();
             switch (b.type) {
             case ADD: 
                 c = lt + rt;
