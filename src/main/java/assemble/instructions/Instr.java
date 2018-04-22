015f7b67f9988c86f242530d97b25eacdf33a317
Ray Zeng <rz72@cornell.edu>
2018-04-22 04:07:34 -0400

    skeleton for assembly cgf

--- src/main/java/assemble/instructions/Instr.java
+++ src/main/java/assemble/instructions/Instr.java
@@ -1,8 +1,15 @@
 package assemble.instructions;
 
 import java.util.List;
+import java.util.UUID;
 
 public abstract class Instr {
+    public Instr() {
+        this.id = UUID.randomUUID();
+    }
+
+    public UUID id;
+
     public abstract List<String> toAbstractAssembly();
     
     public abstract List<String> toAssembly();
---
+++ src/main/java/optimize/ASAEdgeFactory.java
@@ -0,0 +1,13 @@
+package optimize;
+
+import assemble.instructions.Instr;
+import util.PairEdge;
+import util.PairEdgeFactory;
+
+/** Superclass for all IR control flow graph edges. */
+abstract class ASAEdgeFactory<E> implements PairEdgeFactory<Instr, E> {
+    @Override
+    public PairEdge<Instr, E> createEdge(Instr a, Instr b) {
+        return new PairEdge<>(a, b, null);
+    }
+}
