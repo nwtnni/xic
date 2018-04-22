ad1181a59aaecc44b0435a9e71d0d99a97b58128
Ray Zeng <rz72@cornell.edu>
2018-04-22 15:53:03 -0400

    ir cfg -> ir transformation added

--- src/main/java/ir/IRCompUnit.java
+++ src/main/java/ir/IRCompUnit.java
@@ -41,30 +41,6 @@ public class IRCompUnit extends IRNode {
         return "COMPUNIT";
     }
 
-    // @Override
-    // public IRNode visitChildren(IRVisitor v) {
-    //     boolean modified = false;
-
-    //     Map<String, IRFuncDecl> results = new LinkedHashMap<>();
-    //     for (IRFuncDecl func : functions.values()) {
-    //         IRFuncDecl newFunc = (IRFuncDecl) v.visit(this, func);
-    //         if (newFunc != func) modified = true;
-    //         results.put(newFunc.name(), newFunc);
-    //     }
-
-    //     if (modified) return v.nodeFactory().IRCompUnit(name, results);
-
-    //     return this;
-    // }
-
-    // @Override
-    // public <T> T aggregateChildren(AggregateVisitor<T> v) {
-    //     T result = v.unit();
-    //     for (IRFuncDecl func : functions.values())
-    //         result = v.bind(result, v.visit(func));
-    //     return result;
-    // }
-    
     @Override
     public <T> T accept(IRVisitor<T> v) {
         return v.visit(this);
--- src/main/java/optimize/IRGraph.java
+++ src/main/java/optimize/IRGraph.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import ir.*;
 import util.PairEdge;
 import util.PairEdgeGraph;
+import xic.XicInternalException;
 
 /** A IR control flow graph. */
 @SuppressWarnings("serial")
@@ -16,7 +17,25 @@ public class IRGraph<E> extends PairEdgeGraph<IRNode, E> {
         this.name = name;
     }
 
+    /** The name of the function associated with this CFG. */
     private String name;
+
+    /** 
+     * Adds the tail node from the edge in edges to a set of 
+     * visited nodes and the sequence of nodes. 
+     * Requires: edges contains a single edge.
+     * */
+    private IRNode addNode(Set<PairEdge<IRNode, E>> edges, Set<IRNode> visited, IRSeq body) {
+        assert edges.size() == 1;
+        PairEdge<IRNode, E> edge = edges.iterator().next();
+
+        if (visited.contains(edge.tail)) {
+            throw XicInternalException.internal("Trying to add an IR node twice!");
+        }
+        visited.add(edge.tail);
+        body.add(edge.tail);
+        return edge.tail;
+    }
 
     public IRFuncDecl toIR() {
         IRSeq body = new IRSeq();
@@ -28,9 +47,31 @@ public class IRGraph<E> extends PairEdgeGraph<IRNode, E> {
         visited.add(start);
         body.add(start);
 
-        while (remaining.size() > 0) {
-            // take a full trace
+        IRNode current = start;
+        while (current != null) {
+            Set<PairEdge<IRNode, E>> edges = outgoingEdgesOf(current);
+            if (current instanceof IRCJump) {
+                IRCJump cjump = (IRCJump) current;
+
+                // Follow fall through edge
+                PairEdge<IRNode, E> e = getEdge(cjump, cjump.trueLabel());
+                edges.remove(e);
+                current = addNode(edges, visited, body);
+            } else if (current instanceof IRJump) {
+                current = null;
+            } else if (current instanceof IRLabel) {
+                current = addNode(edges, visited, body);
+            } else if (current instanceof IRMove) {
+                current = addNode(edges, visited, body);
+            } else if (current instanceof IRReturn) {
+                current = null;
+            }
+            remaining.removeAll(visited);
+            if (current == null && remaining.size() > 0) {
+                current = remaining.iterator().next();
+            }
         }
+
 
         return fn;
     }
