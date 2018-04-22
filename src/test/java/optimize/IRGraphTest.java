889aaad1b5abeaa0423d9700b4003c5008c6d06d
Ray Zeng <rz72@cornell.edu>
2018-04-22 16:06:11 -0400

    add junit test skeleton

---
+++ src/test/java/optimize/IRGraphTest.java
@@ -0,0 +1,17 @@
+package optimize;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import optimize.*;
+
+public class IRGraphTest {
+	
+	@Test
+	public void testLinear() {
+        assertEquals(true, true);
+    }
+}
