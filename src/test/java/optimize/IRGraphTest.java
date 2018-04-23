1e096acfe57e3cd0677a90c978dd09645b6d9a2d
Ray Zeng <rz72@cornell.edu>
2018-04-23 03:58:26 -0400

    unit test for irgraph

--- src/test/java/optimize/IRGraphTest.java
+++ src/test/java/optimize/IRGraphTest.java
@@ -2,16 +2,35 @@ package optimize;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
+
+import ir.*;
 import optimize.*;
 
 public class IRGraphTest {
-	
+    
+    IREdgeFactory<Void> ef = new IREdgeFactory<>();
+
 	@Test
 	public void testLinear() {
-        assertEquals(true, true);
+        IRFuncDecl fn = new IRFuncDecl("fn");
+        fn.add(new IRMove(new IRTemp("a"), new IRConst(0)));
+        fn.add(new IRMove(new IRTemp("b"), new IRConst(1)));
+        fn.add(new IRMove(new IRTemp("c"), new IRConst(2)));
+        fn.add(new IRReturn());
+
+        IRCompUnit comp = new IRCompUnit("prog");
+        comp.appendFunc(fn);
+
+        IRGraphFactory<Void> gf = new IRGraphFactory<>(comp, ef);
+        Map<String, IRGraph<Void>> cfgs = gf.getCfgs();
+
+        IRFuncDecl after = cfgs.get("fn").toIR();
+        assertEquals(fn.get(0), after.get(0));
+        assertEquals(fn.get(1), after.get(1));
+        assertEquals(fn.get(2), after.get(2));
+
     }
 }
