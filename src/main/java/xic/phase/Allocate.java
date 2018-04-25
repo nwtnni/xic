c662ad2ee0b000c4857d1a3a4fe0085dab71bf40
Newton Ni <nwtnni@gmail.com>
2018-04-25 02:46:26 -0400

    Allocating phase

--- src/main/java/xic/phase/Allocate.java
+++ src/main/java/xic/phase/Allocate.java
@@ -1,6 +1,17 @@
 package xic.phase;
 
+import java.io.IOException;
+import java.io.FileWriter;
+
+import assemble.instructions.CompUnit;
+import assemble.TrivialAllocator;
+
+import util.Filename;
+import util.Pair;
 import util.Result;
+
+import xic.XicException;
+import xic.XicInternalException;
 
 public class Allocate extends Phase {
 
@@ -8,7 +19,30 @@ public class Allocate extends Phase {
 
     @Override
     public Result<Product> process(Config config, Result<Product> previous) {
-        // TODO
-        return null;
+
+        try {
+
+            if (previous.isErr()) return previous;
+
+            CompUnit tiled = previous.ok().getAssembled();
+            tiled = TrivialAllocator.allocate(tiled);
+
+            String out = Filename.concat(config.sink, config.unit);
+            out = Filename.setExtension(out, "s");
+
+            Filename.makePathTo(out);
+            FileWriter w = new FileWriter(out);
+            
+            for (String i : tiled.toAssembly()) {
+                w.append(i + "\n");
+            }
+
+            w.close();
+
+            return new Result<>(Product.assembled(tiled));
+
+        } catch (IOException e) {
+            throw new XicInternalException(e.toString());
+        }
     }
 }
