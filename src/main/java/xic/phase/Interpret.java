c8e9d9bbd57b1e4f4e02fddef1111db893373f36
Newton Ni <nwtnni@gmail.com>
2018-04-25 02:27:52 -0400

    Interpreting phase

--- src/main/java/xic/phase/Interpret.java
+++ src/main/java/xic/phase/Interpret.java
@@ -1,5 +1,12 @@
 package xic.phase;
 
+import interpret.IRSimulator;
+import interpret.IRSimulator.Trap;
+
+import ir.IRCompUnit;
+import emit.ABIContext;
+
+import util.Pair;
 import util.Result;
 
 public class Interpret extends Phase {
@@ -8,7 +15,22 @@ public class Interpret extends Phase {
 
     @Override
     public Result<Product> process(Config config, Result<Product> previous) {
-        // TODO
-        return null;
+
+        if (previous.isErr()) return previous;
+
+        Pair<IRCompUnit, ABIContext> emitted = previous.ok().getEmitted();
+
+        System.out.println("\n--- Intepreting " + config.source + "... ---\n");
+
+        try {
+            IRSimulator sim = new IRSimulator(emitted.first);
+            sim.call("_Imain_paai", 0);
+        } catch (Trap e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("\n");
+
+        return previous;
     }
 }
