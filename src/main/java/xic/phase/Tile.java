eb25480c8dad3dd2c8b18103bc96c6045b331bf6
Newton Ni <nwtnni@gmail.com>
2018-04-25 02:31:16 -0400

    Tiling phase

--- src/main/java/xic/phase/Tile.java
+++ src/main/java/xic/phase/Tile.java
@@ -1,5 +1,12 @@
 package xic.phase;
 
+import ir.IRCompUnit;
+import emit.ABIContext;
+
+import assemble.instructions.CompUnit;
+import assemble.Tiler;
+
+import util.Pair;
 import util.Result;
 
 public class Tile extends Phase {
@@ -8,7 +15,13 @@ public class Tile extends Phase {
 
     @Override
     public Result<Product> process(Config config, Result<Product> previous) {
-        // TODO
-        return null;
+
+        if (previous.isErr()) return previous; 
+
+        Pair<IRCompUnit, ABIContext> emitted = previous.ok().getEmitted(); 
+
+        CompUnit tiled = Tiler.tile(emitted.first, emitted.second);
+        
+        return new Result<>(Product.assembled(tiled));
     }
 }
