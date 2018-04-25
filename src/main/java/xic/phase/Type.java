19e9b8d6e970ff729293ba5227d1972ee06bbdc5
Newton Ni <nwtnni@gmail.com>
2018-04-25 01:10:02 -0400

    Transferred TypeCheck phase logic

--- src/main/java/xic/phase/Type.java
+++ src/main/java/xic/phase/Type.java
@@ -1,6 +1,16 @@
 package xic.phase;
 
+import java.io.IOException;
+import java.io.FileWriter;
+
+import ast.Program;
+import type.TypeChecker;
+
+import util.Filename;
 import util.Result;
+
+import xic.XicException;
+import xic.XicInternalException;
 
 public class Type extends Phase {
 
@@ -8,8 +18,45 @@ public class Type extends Phase {
 
     @Override
     public Result<Intermediate> process(Config config, Result<Intermediate> previous) {
-        // TODO
-        return null;
-    }
 
+        String ext = Filename.getExtension(config.unit);
+        String out = Filename.concat(config.sink, config.unit);
+        out = Filename.setExtension(out, "typed");
+
+        try {
+            try {
+
+                if (previous.isErr()) throw previous.err();
+
+                if (!ext.equals("xi")) throw XicException.unsupported(config.unit);
+
+                Program ast = previous.ok().getAST();
+
+                TypeChecker.check(config.lib, ast);
+
+                if (output) {
+                    Filename.makePathTo(out);
+                    FileWriter writer = new FileWriter(out);
+                    writer.write("Valid Xi Program");
+                    writer.close();
+                }
+
+                return new Result<>(new Intermediate(ast));
+
+            } catch (XicException e) {
+
+                if (output) {
+                    Filename.makePathTo(out);
+                    FileWriter writer = new FileWriter(out);
+                    writer.write(e.toWrite());
+                    writer.close();
+                }
+
+                return new Result<>(e);
+            }
+
+        } catch (IOException e) {
+            throw new XicInternalException(e.toString());
+        }
+    }
 }
