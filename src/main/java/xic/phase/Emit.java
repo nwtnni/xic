40c73228edfdc7df0e27da4593a9c8d120b80551
Newton Ni <nwtnni@gmail.com>
2018-04-25 01:58:56 -0400

    Emit phase

--- src/main/java/xic/phase/Emit.java
+++ src/main/java/xic/phase/Emit.java
@@ -1,6 +1,17 @@
 package xic.phase;
 
+import ast.Program;
+import type.FnContext;
+
+import emit.ABIContext;
+import emit.Emitter;
+import ir.IRCompUnit;
+
+import util.Filename;
+import util.Pair;
 import util.Result;
+
+import xic.XicException;
 
 public class Emit extends Phase {
 
@@ -8,7 +19,16 @@ public class Emit extends Phase {
 
     @Override
     public Result<Product> process(Config config, Result<Product> previous) {
-        // TODO
-        return null;
+
+        if (previous.isErr()) return previous;
+        
+        Pair<Program, FnContext> typed = previous.ok().getTyped();
+
+        try {
+            Pair<IRCompUnit, ABIContext> result = Emitter.emitIR(typed.first, typed.second);
+            return new Result<>(Product.emitted(result));
+        } catch (XicException e) {
+            return new Result<>(e);
+        }
     }
 }
