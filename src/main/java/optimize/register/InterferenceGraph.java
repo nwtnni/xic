d2fee2e7bb8dc22a9aa22a337b73a33b8bdaf2b3
Ray Zeng <rz72@cornell.edu>
2018-04-26 07:59:24 -0400

    fixed renamed field in interference graph

--- src/main/java/optimize/register/InterferenceGraph.java
+++ src/main/java/optimize/register/InterferenceGraph.java
@@ -26,7 +26,7 @@ public class InterferenceGraph {
     
         for (Instr instr : instructions) {
         
-            List<Temp> live = new ArrayList<>(instr.live);
+            List<Temp> live = new ArrayList<>(instr.in);
             int size = live.size();
 
             for (int i = 0; i < size; i++) {
