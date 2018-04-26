da6408878c914034c6e4043785515d4c29b2245f
Newton Ni <nwtnni@gmail.com>
2018-04-26 16:22:31 -0400

    Added methods to Allocator skeleton

--- src/main/java/optimize/register/Allocator.java
+++ src/main/java/optimize/register/Allocator.java
@@ -1,11 +1,50 @@
 package optimize.register;
 
+import java.util.Set;
+import java.util.Optional;
+
+import assemble.Temp;
+import assemble.Operand;
+import assemble.FuncDecl;
 import assemble.CompUnit;
+import assemble.instructions.Instr;
 
 public class Allocator {
+
+    private static final Set<Operand> available = Set.of(
+        Operand.R10,
+        Operand.R11
+    );
 
     public static CompUnit allocate(CompUnit unit) {
 
         return unit;
     }
+
+    private FuncDecl fn;
+    private Stack<Temp> popped;
+
+    private Allocator(FuncDecl fn) {
+        this.fn = fn; 
+    }
+    
+    // Main try -> spill loop
+    private FuncDecl process() {
+
+    }
+
+    // Returns empty if colorable with spills
+    // Otherwise false and must spill the returned Temp
+    private Optional<Temp> tryAllocate() {
+
+
+    }
+
+    // Mutates FuncDecl fn to spill the temp
+    // Must rerun live variable analysis, reconstruct Interference && Color graphs
+    private void spill(Temp t) {
+
+    }
+    
+    
 }
