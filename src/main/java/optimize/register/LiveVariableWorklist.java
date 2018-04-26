a0eabe6e2a97ac8953495065d7a9951c3f4a053b
Ray Zeng <rz72@cornell.edu>
2018-04-26 01:17:21 -0400

    wrote LV worklist and added pass

--- src/main/java/optimize/register/LiveVariableWorklist.java
+++ src/main/java/optimize/register/LiveVariableWorklist.java
@@ -17,25 +17,35 @@ public class LiveVariableWorklist extends Worklist<ASAGraph<Set<Temp>>, Instr, S
     }
 
     /** 
-     * Takes the result of the meet [m] and applys the transfer function
-     * at node [v].
+     * The transfer function is use[n] union (out[n] - def[n])
      */
-    public Set<Temp> transfer(Set<Temp> m, Instr v) {
-        return null;
+    public Set<Temp> transfer(Set<Temp> out, Instr v) {
+        Set<Temp> in = v.use;
+        out.removeAll(v.def);
+        in.addAll(out);
+        return in;
     }
 
     /** 
-     * Meet function takes a set of the paths [paths] and merges 
-     * them with the meet operator.
+     * The meet operation is Union(in[n'])
      */
-    public Set<Temp> meet(Set<PairEdge<Instr,Set<Temp>>> paths) {
-        return null;
+    public Set<Temp> meet(Set<PairEdge<Instr,Set<Temp>>> inPaths) {
+        Set<Temp> out = new HashSet<>();
+        for (PairEdge<Instr, Set<Temp>> path : inPaths) {
+            out.addAll(path.value);
+        }
+        return out;
     }
 
     /**
-     * Annotates a node [v] with the result of the meet [e].
+     * Annotates a node [v] with the set of live variables 
+     * (the out set defined by the meet)
+     * Returns true if set of live variables has changed.
      */
-    public boolean annotate(Instr v, Set<Temp> e) {
-        return false;
+    public boolean annotate(Instr v, Set<Temp> out) {
+        if (v.live.containsAll(out) && out.containsAll(v.live)) {
+            return false;
+        }
+        return true;
     }
 }
