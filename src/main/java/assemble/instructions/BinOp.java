f0e13fedf92592eb9cd818e8d4c09b6ad1a99fe8
Newton Ni <nwtnni@gmail.com>
2018-04-27 06:01:55 -0400

    Lifting BinOp addressing modes into type system

--- src/main/java/assemble/instructions/BinOp.java
+++ src/main/java/assemble/instructions/BinOp.java
@@ -2,58 +2,123 @@ package assemble.instructions;
 
 import assemble.*;
 
-public class BinOp extends Instr {
+import util.Pair;
+
+public class BinOp<L, R, T> extends Instr<T> {
+
     public enum Kind {
         ADD     ("addq"),
         SUB     ("subq"),
         AND     ("andq"),
         OR      ("orq"),
-        XOR     ("xorq"),
-        LSHIFT  ("shlq"),
-        RSHIFT  ("shrq"),
-        ARSHIFT ("sarq");
+        XOR     ("xorq");
 
         String opcode;
         private Kind (String s) { opcode = s; }
     }
 
     public Kind kind;
-    public Temp destTemp;
-    public Temp srcTemp;
-
-    public Operand dest;
-    public Operand src;
+    public L src;
+    public R dest;
 
     /**
-     * Creates a BinOp with abstract operands.
+     * Creates a BinOp with operands.
      */
-    public BinOp(Kind kind, Temp d, Temp s) {
+    private BinOp(Kind kind, L src, R dest) {
         this.kind = kind;
-        this.destTemp = d;
-        this.srcTemp = s;
-    }
-
-    /** 
-     * Create a BinOp with real operands. Should not be used until allocation.
-     */
-    public BinOp(Kind kind, Operand dest, Operand src) {
-        this.kind = kind;
-        this.dest = dest;
         this.src = src;
+        this.dest = dest;
     }
 
     @Override
-    public String toAbstractAssembly() {
-        return String.format("%s %s, %s", kind.opcode, srcTemp, destTemp);
-    }
-
-    @Override
-    public String toAssembly() {
+    public String toString() {
         return String.format("%s %s, %s", kind.opcode, src, dest);
     }
 
     @Override
     public <T> T accept(InsVisitor<T> v) {
         return v.visit(this);
+    }
+
+    /*
+     *
+     * Abstract Assembly Instructions
+     *
+     */
+
+    /**
+     * Abstract immediate source, register destination addressing mode.
+     */
+    public static class AIR extends BinOp<Imm, Temp, Temp> {
+        public AIR(Kind kind, Imm src, Temp dest) { super(kind, src, dest); }
+    }
+
+    /**
+     * Abstract immediate source, memory destination addressing mode.
+     */
+    public static class AIM extends BinOp<Imm, Mem<Temp>, Temp> {
+        public AIM(Kind kind, Imm src, Mem<Temp> dest) { super(kind, src, dest); }
+    }
+
+    /**
+     * Abstract register source, memory destination addressing mode.
+     */
+    public static class ARM extends BinOp<Temp, Mem<Temp>, Temp> {
+        public ARM(Kind kind, Temp src, Mem<Temp> dest) { super(kind, src, dest); }
+    }
+
+    /**
+     * Abstract memory source, register destination addressing mode.
+     */
+    public static class AMR extends BinOp<Mem<Temp>, Temp, Temp> {
+        public AMR(Kind kind, Mem<Temp> src, Temp dest) { super(kind, src, dest); }
+    }
+
+    /**
+     * Abstract register source, register destination addressing mode.
+     */
+    public static class ARR extends BinOp<Temp, Temp, Temp> {
+        public ARR(Kind kind, Temp src, Temp dest) { super(kind, src, dest); }
+    }
+
+    /*
+     *
+     * Assembly Instructions
+     *
+     */
+
+    /**
+     * Immediate source, register destination addressing mode.
+     */
+    public static class IR extends BinOp<Imm, Reg, Reg> {
+        public IR(Kind kind, Imm src, Reg dest) { super(kind, src, dest); }
+    }
+
+    /**
+     * Immediate source, memory destination addressing mode.
+     */
+    public static class IM extends BinOp<Imm, Mem<Reg>, Reg> {
+        public IM(Kind kind, Imm src, Mem<Reg> dest) { super(kind, src, dest); }
+    }
+
+    /**
+     * Register source, memory destination addressing mode.
+     */
+    public static class RM extends BinOp<Reg, Mem<Reg>, Reg> {
+        public RM(Kind kind, Reg src, Mem<Reg> dest) { super(kind, src, dest); }
+    }
+
+    /**
+     * Memory source, register destination addressing mode.
+     */
+    public static class MR extends BinOp<Mem<Reg>, Reg, Reg> {
+        public MR(Kind kind, Mem<Reg> src, Reg dest) { super(kind, src, dest); }
+    }
+
+    /**
+     * Register source, register destination addressing mode.
+     */
+    public static class RR extends BinOp<Reg, Reg, Reg> {
+        public RR(Kind kind, Reg src, Reg dest) { super(kind, src, dest); }
     }
 }
