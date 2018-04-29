d8e1fc5769f394d9fbe508dbdfdd9a39f68e07bc
Newton Ni <nwtnni@gmail.com>
2018-04-29 18:35:03 -0400

    Either equals and hashCode

--- src/main/java/util/Either.java
+++ src/main/java/util/Either.java
@@ -41,6 +41,27 @@ public class Either<L, R> {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Either<?, ?>)) return false;
+
+        Either<?, ?> e = (Either<?, ?>) o;
+        if (isLeft() && e.isLeft()) {
+            return getLeft().equals(e.getLeft());
+        } else if (isRight() && e.isRight()) {
+            return getRight().equals(e.getRight());
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        // GUARANTEED TO BE PRIME
+        int prime = (isLeft()) ? 1282519 : 9309831; 
+        return (isLeft()) ? getLeft().hashCode() ^ prime : getRight().hashCode() ^ prime; 
+    }
+
+    @Override
     public String toString() {
         return String.format("%s of %s, %s", kind, left, right);
     }
