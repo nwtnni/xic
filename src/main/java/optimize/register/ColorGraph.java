6f2154339096ef2f955dfca63aa0878e2a960e98
Newton Ni <nwtnni@gmail.com>
2018-04-29 22:19:00 -0400

    Try actually coloring the graph

--- src/main/java/optimize/register/ColorGraph.java
+++ src/main/java/optimize/register/ColorGraph.java
@@ -171,6 +171,8 @@ public class ColorGraph {
             }
         }
 
+        assignColors();
+
         if (this.spilledNodes.isEmpty()) {
             return Either.left(this.color);
         } else {
